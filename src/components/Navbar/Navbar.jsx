a9ff31114e776abb2b3a512eb2b0f3f5bed7964b
salah eddine sabir <salaheddinesabirofficial@gmail.com>
2022-01-08 11:29:46 +0100

    save

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
                 </div>
                 <div className="navmiddle">
                     <ul className="navmenulink">
-                        <li className="navmenuitem"><NavLink activeClassName="active" to="/">Home</NavLink></li>
+                        <li className="navmenuitem"><NavLink activeClassName="active" to="/example-1">Home</NavLink></li>
                         <li className="navmenuitem"><NavLink activeClassName="active" to="/Moreinfo">About Us</NavLink></li>
                         <li className="navmenuitem"><NavLink activeClassName="active" to="/Moreinfo">Contact Us</NavLink></li>
                         <li className="navmenuitem"><NavLink activeClassName="active" to="/Moreinfo">careers</NavLink></li>
