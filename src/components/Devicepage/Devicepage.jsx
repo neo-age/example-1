d56bfdd501567fe4b115bfbae0be460de3da4d8e
salah eddine sabir <salaheddinesabirofficial@gmail.com>
2022-02-07 09:37:26 +0100

    save

--- src/components/Devicepage/Devicepage.jsx
+++ src/components/Devicepage/Devicepage.jsx
@@ -1,11 +1,11 @@
 import './Devicepage.css'
-import salahLogo from '../Devicepage/zdlogo-black.svg';
+import projectLogo from '../Devicepage/zdlogo-black.svg';
 
 function Devicepage() {
     return (
         <div className='devicepage'>
             <div className="devicepage_wrapper">
-                <img src={salahLogo} alt="" className="page_logo" />
+                <img src={projectLogo} alt="" className="page_logo" />
                 <p className="page_paragraph">i'm afraid this project only works on desktop/computers for the time being, i appreciate you taking the time to come by, you can go back to the portfolio with the button in the bottom.</p>
                 <button className="page_button"><a href="https://www.salaheddinesabir.com/">Home</a></button>
             </div>            
